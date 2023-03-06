a94d496e1e2a1d321dc2ecdf5a6a496cfc94f95d
Aaron Gabriel Morales <119905727+deliriousxs@users.noreply.github.com>
2023-03-06 18:03:14 +0800

    Create App.java

---
+++ project/App.java
@@ -0,0 +1,14 @@
+package game;
+import java.awt.EventQueue;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+       
+
+            new MainMenu();
+        });
+    }
+}
